3eb87260fb6d681246dfe4c865df408e278b58b8
dadalton <dakota.dalton@hey.com>
2022-12-18 15:49:09 -0500

    18 dec

--- Ch3_notes-Compound_data_types.rs
+++ Ch3_notes-Compound_data_types.rs
@@ -10,32 +10,32 @@
 
 Two key building blocks: struct and enum
 */
-/*
-#![allow(unused_variables)] // relax compiler warnings
 
-    type File = String;     // type alias
+// #![allow(unused_variables)] // relax compiler warnings
 
-    fn open(f: &mut File) -> bool {
-        true
-    }
+//     type File = String;     // type alias
 
-    fn close(f: &mut File) -> bool {
-        true
-    }
+//     fn open(f: &mut File) -> bool {
+//         true
+//     }
 
-#[allow(dead_code)]         // relax compiler warning about unused functions
-fn read(f: &mut File,
-        save_to: &mut Vec<u8>) -> ! { // ! type indicates the function never returns
-    unimplemented!()        // macro that crashes if encountered 
-}
+//     fn close(f: &mut File) -> bool {
+//         true
+//     }
 
-fn main() {
-    let mut f1 = File::from("f1.txt"); // File inherits String's methods through type alias
-    open(&mut f1);
-    //read(f1, vec![]);
-    close(&mut f1);
-}
-*/
+// #[allow(dead_code)]         // relax compiler warning about unused functions
+// fn read(f: &mut File,
+//         save_to: &mut Vec<u8>) -> ! { // ! type indicates the function never returns
+//     unimplemented!()        // macro that crashes if encountered 
+// }
+
+// fn main() {
+//     let mut f1 = File::from("f1.txt"); // File inherits String's methods through type alias
+//     open(&mut f1);
+//     //read(f1, vec![]);
+//     close(&mut f1);
+// }
+
 // UNIT TYPE () - formally a zero-length tuple
 // - expresses a function that returns no value
 // - eg functions without a return type return (), as do expressions with a ;
@@ -46,140 +46,140 @@ fn main() {
 
 // struct allows creation of a composite type 
 // - analagous to object or record in other languages
-/*
-#![allow(unused_variables)]
 
-    #[derive(Debug)]        // allows println! to print File
-#![allow(unused_variables)]
+// #![allow(unused_variables)]
 
-    #[derive(Debug)]        // allows println! to print File
-    struct File {
-        name: String,   // fields require explicit lifetimes if they're a ref to another object
-        data: Vec<u8>,  // here the field lifetimes are implicit
-    }
+//     #[derive(Debug)]        // allows println! to print File
+// #![allow(unused_variables)]
 
-    fn open(f: &mut File) -> bool {
-        true
-    }
+//     #[derive(Debug)]        // allows println! to print File
+//     struct File {
+//         name: String,   // fields require explicit lifetimes if they're a ref to another object
+//         data: Vec<u8>,  // here the field lifetimes are implicit
+//     }
 
-    fn close(f: &mut File) -> bool {
-        true
-    }
+//     fn open(f: &mut File) -> bool {
+//         true
+//     }
 
-    fn read(
-        f: &File,
-        save_to: &mut Vec<u8>,
-    ) -> usize {
-        let mut tmp = f.data.clone();
-        let read_length = tmp.len();
+//     fn close(f: &mut File) -> bool {
+//         true
+//     }
+
+//     fn read(
+//         f: &File,
+//         save_to: &mut Vec<u8>,
+//     ) -> usize {
+//         let mut tmp = f.data.clone();
+//         let read_length = tmp.len();
     
-        save_to.reserve(read_length); // reserve capacity for read_length amount of elements
-        save_to.append(&mut tmp);
-        read_length
-    }
+//         save_to.reserve(read_length); // reserve capacity for read_length amount of elements
+//         save_to.append(&mut tmp);
+//         read_length
+//     }
 
-    fn open(f: &mut File) -> bool {
-        true
-    }
+//     fn open(f: &mut File) -> bool {
+//         true
+//     }
 
-    fn close(f: &mut File) -> bool {
-        true
-    }
+//     fn close(f: &mut File) -> bool {
+//         true
+//     }
 
-    fn read(
-        f: &File,
-        save_to: &mut Vec<u8>,
-    ) -> usize {
-        let mut tmp = f.data.clone();
-        let read_length = tmp.len();
+//     fn read(
+//         f: &File,
+//         save_to: &mut Vec<u8>,
+//     ) -> usize {
+//         let mut tmp = f.data.clone();
+//         let read_length = tmp.len();
     
-        save_to.reserve(read_length); // reserve capacity for read_length amount of elements
-        save_to.append(&mut tmp);
-        read_length
-    }
+//         save_to.reserve(read_length); // reserve capacity for read_length amount of elements
+//         save_to.append(&mut tmp);
+//         read_length
+//     }
 
-    fn main() {
-        let mut f2 = File {
-            name: String::from("2.txt"),
-            data: vec![114, 117, 115, 116, 33],
-        let mut f2 = File {
-            name: String::from("2.txt"),
-            data: vec![114, 117, 115, 116, 33],
-        };
+//     fn main() {
+//         let mut f2 = File {
+//             name: String::from("2.txt"),
+//             data: vec![114, 117, 115, 116, 33],
+//         let mut f2 = File {
+//             name: String::from("2.txt"),
+//             data: vec![114, 117, 115, 116, 33],
+//         };
 
-        let mut buffer: Vec<u8> = vec![];
+//         let mut buffer: Vec<u8> = vec![];
 
-        open(&mut f2);
-        let f2_length = read(&f2, &mut buffer);
-        close(&mut f2);
+//         open(&mut f2);
+//         let f2_length = read(&f2, &mut buffer);
+//         close(&mut f2);
 
-        let text = String::from_utf8_lossy(&buffer); // convert Vec to String, non-UTF-8 bytes replaced with ?
-        let mut buffer: Vec<u8> = vec![];
+//         let text = String::from_utf8_lossy(&buffer); // convert Vec to String, non-UTF-8 bytes replaced with ?
+//         let mut buffer: Vec<u8> = vec![];
 
-        open(&mut f2);
-        let f2_length = read(&f2, &mut buffer);
-        close(&mut f2);
+//         open(&mut f2);
+//         let f2_length = read(&f2, &mut buffer);
+//         close(&mut f2);
 
-        let text = String::from_utf8_lossy(&buffer); // convert Vec to String, non-UTF-8 bytes replaced with ?
+//         let text = String::from_utf8_lossy(&buffer); // convert Vec to String, non-UTF-8 bytes replaced with ?
 
-        println!("{:?}", f2);
-        println!("{} is {} bytes long", &f2.name, f2_length);
-        println!("{}", text)
-        println!("{:?}", f2);
-        println!("{} is {} bytes long", &f2.name, f2_length);
-        println!("{}", text)
-    }
-*/
-    //     let f1 = File {
-    //         name: String::from("f1.txt"),  // generate owned strings from string literals
-    //         data: Vec::new(),              // simulate empty file
-    //     };
+//         println!("{:?}", f2);
+//         println!("{} is {} bytes long", &f2.name, f2_length);
+//         println!("{}", text)
+//         println!("{:?}", f2);
+//         println!("{} is {} bytes long", &f2.name, f2_length);
+//         println!("{}", text)
+//     }
 
-    //     let f1_name = &f1.name;            // accessing fields by reference prevents use after move
-    //     let f1_length = &f1.data.len();
+//         let f1 = File {
+//             name: String::from("f1.txt"),  // generate owned strings from string literals
+//             data: Vec::new(),              // simulate empty file
+//         };
 
-    //     println!("{:?}", f1);
-    //     println!("{} is {} bytes long", f1_name, f1_length);
-    // }
+//         let f1_name = &f1.name;            // accessing fields by reference prevents use after move
+//         let f1_length = &f1.data.len();
+
+//         println!("{:?}", f1);
+//         println!("{} is {} bytes long", f1_name, f1_length);
+//     }
     
-    //     let f1 = File {
-    //         name: String::from("f1.txt"),  // generate owned strings from string literals
-    //         data: Vec::new(),              // simulate empty file
-    //     };
+//         let f1 = File {
+//             name: String::from("f1.txt"),  // generate owned strings from string literals
+//             data: Vec::new(),              // simulate empty file
+//         };
 
-    //     let f1_name = &f1.name;            // accessing fields by reference prevents use after move
-    //     let f1_length = &f1.data.len();
+//         let f1_name = &f1.name;            // accessing fields by reference prevents use after move
+//         let f1_length = &f1.data.len();
 
-    //     println!("{:?}", f1);
-    //     println!("{} is {} bytes long", f1_name, f1_length);
-    // }
+//         println!("{:?}", f1);
+//         println!("{} is {} bytes long", f1_name, f1_length);
+//     }
     
 // NEWTYPE PATTERN
 // wrap a core type with a struct to create a new type
 // wrap a core type with a struct to create a new type
-/*
-  struct Hostname(String); // hostname will be our new type
 
-    fn connect(host: Hostname) {             // creates host of type Hostname
-        println!("connected to {}", host.0); // access host's data
-    }
+//   struct Hostname(String); // hostname will be our new type
 
-    fn main() {
-        let ordinary_string = String::from("localhost"); // get the string
-        let host = Hostname ( ordinary_string.clone() ); // create a Hostname type using a copy of the string
-  struct Hostname(String); // hostname will be our new type
+//     fn connect(host: Hostname) {             // creates host of type Hostname
+//         println!("connected to {}", host.0); // access host's data
+//     }
 
-    fn connect(host: Hostname) {             // creates host of type Hostname
-        println!("connected to {}", host.0); // access host's data
-    }
+//     fn main() {
+//         let ordinary_string = String::from("localhost"); // get the string
+//         let host = Hostname ( ordinary_string.clone() ); // create a Hostname type using a copy of the string
+//   struct Hostname(String); // hostname will be our new type
 
-    fn main() {
-        let ordinary_string = String::from("localhost"); // get the string
-        let host = Hostname ( ordinary_string.clone() ); // create a Hostname type using a copy of the string
+//     fn connect(host: Hostname) {             // creates host of type Hostname
+//         println!("connected to {}", host.0); // access host's data
+//     }
 
-        connect(ordinary_string); // this will generate an error as it is a String and not a Hostname type
-}
-*/
+//     fn main() {
+//         let ordinary_string = String::from("localhost"); // get the string
+//         let host = Hostname ( ordinary_string.clone() ); // create a Hostname type using a copy of the string
+
+//         connect(ordinary_string); // this will generate an error as it is a String and not a Hostname type
+// }
+
 
 // ------ 3.3 Adding methods to a struct with impl --------
 /* 
@@ -187,7 +187,7 @@ fn main() {
 - Syntactically speaking, they are functions that don't need to specify one of their arguments
     - e.g. read(f, buffer) can be simplified as f.read(buffer)
 - Rust does not contain the class keyword - unlike other languages that support methods
-    - struct and enum can seem like classes but do not support injeritance
+    - struct and enum can seem like classes but do not support inheritance
 - methods are defined with impl blocks, which are distinct from struc/enum
 
 - Rust:
@@ -216,34 +216,34 @@ fn main() {
 - File::new() syntax:
     File::new("f1.txt", vec![]);
 */
-/*
-#[derive(Debug)]
-    struct File {
-        name: String,
-        data: Vec<u8>,
-    }
 
-    impl File {
-        fn new(name: &str) -> File {
-            File {
-                name: String::from(name),
-                data: Vec::new(),
-            }
-        }
-    }
+// #[derive(Debug)]
+//     struct File {
+//         name: String,
+//         data: Vec<u8>,
+//     }
 
-    fn main() {
-        let f3 = File::new("f3.txt");
+//     impl File {
+//         fn new(name: &str) -> File {
+//             File {
+//                 name: String::from(name),
+//                 data: Vec::new(),
+//             }
+//         }
+//     }
 
-        let f3_name = &f3.name;
-        let f3_length = f3.data.len();
+//     fn main() {
+//         let f3 = File::new("f3.txt");
 
-        println!("{:?}", f3);
-        println!("{} is {} bytes long", f3_name, f3_length);
-    }
-    connect(ordinary_string);
+//         let f3_name = &f3.name;
+//         let f3_length = f3.data.len();
 
-*/
+//         println!("{:?}", f3);
+//         println!("{} is {} bytes long", f3_name, f3_length);
+//     }
+//     connect(ordinary_string);
+
+
 
 // ------ 3.4 RETURNING ERRORS ------
 /*
--- advent/advent1.rs
+++ advent/advent1.rs
@@ -12,7 +12,7 @@ use regex::Regex;
 fn main() {
     let mut f = File::open("caloriesample.txt")?;
     let mut reader = BufReader::new(f);
-    let mut buffer = String::new();
+    let mut buffer = f.len(); //String::new();
 
     let re = Regex::new(r"(\n)").unwrap();
     let count: i32 = 0;
@@ -41,100 +41,100 @@ fn main() {
 // newline counter - if newline == 2, store in array as total calorie
 
 
- for (let i = 0; i < 10; i++) {
-    // code to execute 10 times
-  }
-
-
-
-
- 
- #[derive(Debug)]
- pub struct File {
-   name: String,
-   data: Vec<u8>,
- }
- 
- impl File {
-   /// New files are assumed to be empty, but a name is required.
-   pub fn new(name: &str) -> File {
-     File {
-       name: String::from(name),
-       data: Vec::new(),
-     }
-   }
- 
-   /// Returns the file's length in bytes.
-   pub fn len(&self) -> usize {
-     self.data.len()
-   }
- 
-   /// Returns the file's name.
-   pub fn name(&self) -> String {
-     self.name.clone()
-   }
- }
- 
-// // Objects
-// struct MyStruct {
-//   // properties
-//   name: String,
-//   age: i32,
-// }
-
-// // methods
-// impl MyStruct {
-//   fn print_info(&self) {
-//       println!("Name: {}, Age: {}", self.name, self.age);
+//  for (let i = 0; i < 10; i++) {
+//     // code to execute 10 times
 //   }
+
+
+
+
+ 
+//  #[derive(Debug)]
+//  pub struct File {
+//    name: String,
+//    data: Vec<u8>,
+//  }
+ 
+//  impl File {
+//    /// New files are assumed to be empty, but a name is required.
+//    pub fn new(name: &str) -> File {
+//      File {
+//        name: String::from(name),
+//        data: Vec::new(),
+//      }
+//    }
+ 
+//    /// Returns the file's length in bytes.
+//    pub fn len(&self) -> usize {
+//      self.data.len()
+//    }
+ 
+//    /// Returns the file's name.
+//    pub fn name(&self) -> String {
+//      self.name.clone()
+//    }
+//  }
+ 
+// // // Objects
+// // struct MyStruct {
+// //   // properties
+// //   name: String,
+// //   age: i32,
+// // }
+
+// // // methods
+// // impl MyStruct {
+// //   fn print_info(&self) {
+// //       println!("Name: {}, Age: {}", self.name, self.age);
+// //   }
+// // }
+
+// // // creating an object
+// // let obj = MyStruct {
+// //   name: "John Doe".to_string(),
+// //   age: 30,
+// // };
+
+// //  fn main() {
+// //    let f1 = File::new("f1.txt");
+ 
+// //    let f1_name = f1.name();
+// //    let f1_length = f1.len();
+ 
+// //    println!("{:?}", f1);
+// //    println!("{} is {} bytes long", f1_name, f1_length);
+// //  }
+
+
+//  for (let i = 0; i < 10; i++) {
+//   // code to execute 10 times
 // }
 
-// // creating an object
-// let obj = MyStruct {
-//   name: "John Doe".to_string(),
-//   age: 30,
-// };
+// while (condition) {
+//   // code to execute until condition is false
+// }
 
-//  fn main() {
-//    let f1 = File::new("f1.txt");
- 
-//    let f1_name = f1.name();
-//    let f1_length = f1.len();
- 
-//    println!("{:?}", f1);
-//    println!("{} is {} bytes long", f1_name, f1_length);
-//  }
+//  // Declare a struct called "Person" that has two fields: "name" and "age"
+// struct Person {
+//   name: String,
+//   age: u8,
+// }
 
+// // Define a function called "greet" that takes a "Person" instance as an argument
+// // and returns a string
+// fn greet(person: Person) -> String {
+//   // Use the "format!" macro to create a string containing the person's name and age
+//   format!("Hello, my name is {} and I am {} years old.", person.name, person.age)
+// }
 
- for (let i = 0; i < 10; i++) {
-  // code to execute 10 times
-}
+// fn main() {
+//   // Create a new instance of the "Person" struct, using field shorthand syntax
+//   let person = Person { name: "John Doe".to_string(), age: 30 };
 
-while (condition) {
-  // code to execute until condition is false
-}
+//   // Call the "greet" function, passing the "person" instance as an argument,
+//   // and store the result in a variable called "message"
+//   let message = greet(person);
 
- // Declare a struct called "Person" that has two fields: "name" and "age"
-struct Person {
-  name: String,
-  age: u8,
-}
-
-// Define a function called "greet" that takes a "Person" instance as an argument
-// and returns a string
-fn greet(person: Person) -> String {
-  // Use the "format!" macro to create a string containing the person's name and age
-  format!("Hello, my name is {} and I am {} years old.", person.name, person.age)
-}
-
-fn main() {
-  // Create a new instance of the "Person" struct, using field shorthand syntax
-  let person = Person { name: "John Doe".to_string(), age: 30 };
-
-  // Call the "greet" function, passing the "person" instance as an argument,
-  // and store the result in a variable called "message"
-  let message = greet(person);
-
-  // Use the "println!" macro to print the "message" to the console
-  println!("{}", message);
-}
+//   // Use the "println!" macro to print the "message" to the console
+//   println!("{}", message);
+// }
